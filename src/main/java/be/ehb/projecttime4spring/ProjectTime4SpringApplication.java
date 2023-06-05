666d7cc590f82f8c0f99f1a293b4f2a0b31b7f3f
Mat. Devski Carlier <113537267+matecarl@users.noreply.github.com>
2023-06-05 14:37:23 +0200

    initial project initialisation

---
+++ src/main/java/be/ehb/projecttime4spring/ProjectTime4SpringApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.projecttime4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTime4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTime4SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/projecttime4spring/ProjectTime4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.projecttime4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTime4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
